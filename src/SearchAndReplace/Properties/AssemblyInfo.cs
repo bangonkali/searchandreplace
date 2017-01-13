72186125f5fdf719b0098ad5bc6f4892962b6efa
bangonkali <bangonkali@gmail.com>
2017-01-13 14:26:58 +0800

    Modified assembly info.

--- src/SearchAndReplace/Properties/AssemblyInfo.cs
+++ src/SearchAndReplace/Properties/AssemblyInfo.cs
@@ -2,13 +2,22 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bangon Kali")]
+[assembly: AssemblyCopyright("Copyright © 2017 Bangon Kali")]
 [assembly: AssemblyProduct("SearchAndReplace")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("SearchAndReplace")]
+[assembly: AssemblyTitle("SearchAndReplace")]
+[assembly: AssemblyDescription("A very simple search and replace tool that does search and replace on folder names, file names and contents of files within the root folder and doing so recursively.")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
